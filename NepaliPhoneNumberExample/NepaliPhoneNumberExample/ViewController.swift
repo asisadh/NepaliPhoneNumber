5c81508d88c11bb100f06ca10b38e146bb622fb0
Aashish Adhikari <asis.adh@gmail.com>
2018-09-07 12:56:14 +0545

    Adding Example Code

--- NepaliPhoneNumberExample/NepaliPhoneNumberExample/ViewController.swift
+++ NepaliPhoneNumberExample/NepaliPhoneNumberExample/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        testNepaliPhoneNumberTest()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +20,50 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    private func testNepaliPhoneNumberTest(){
+        
+        let aPhone = "9845045191"
+        let bPhone = "9856022530"
+        let cPhone = "9801031117"
+        let dPhone = "9741151609"
+        
+        let aLandline = "14350994"
+        let bLandline = "83551338"
+        
+        // For Phone
+        // Check is valid Phone number or not
+        print(NepaliPhoneNumber.isValid(phone: aPhone))
+        print(NepaliPhoneNumber.isValid(phone: bPhone))
+        print(NepaliPhoneNumber.isValid(phone: bLandline))
+        
+        // Print formatted String and can supply any formatter character
+        // May be space or a hyphen
+        // Your choice
+        print(NepaliPhoneNumber.formattedNumber(phone: aPhone, formatter: " "))
+        print(NepaliPhoneNumber.formattedNumber(phone: bLandline, formatter: " "))
+        
+        // For Mobile
+        // Verify Mobile Number
+        print(NepaliPhoneNumber.isValid(phone: cPhone))
+        print(NepaliPhoneNumber.isValid(phone: dPhone))
+        
+        // Can fetch the Name of Company and Type of Sim from the given number.
+        print(NepaliPhoneNumber.formattedNumber(mobile: cPhone, formatter: " "))
+        print(NepaliPhoneNumber.getTheNumberDetail(mobile: cPhone)?.nameOfCompany.name)
+        print(NepaliPhoneNumber.getTheNumberDetail(mobile: cPhone)?.typeOfNumber.name)
+        print(NepaliPhoneNumber.getTheNumberDetail(mobile: dPhone)?.nameOfCompany.name)
+        print(NepaliPhoneNumber.getTheNumberDetail(mobile: dPhone)?.typeOfNumber.name)
+        
+        // For Land Line
+        // Area Code also needed to be passed to get verified.
+        print(NepaliPhoneNumber.isValid(phone: aLandline))
+        print(NepaliPhoneNumber.isValid(phone: bLandline))
+        
+        // Print formatted LandLine Number
+        print(NepaliPhoneNumber.formattedNumber(landLine: aLandline, formatter: "-"))
+        print(NepaliPhoneNumber.formattedNumber(landLine: bLandline, formatter: "-"))
+        
+    }
 
 }
 
